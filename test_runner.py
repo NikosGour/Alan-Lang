cc2fd50827ebf9afd58e6c8f44144120f7525c08
NikosGour <nikosgournak@gmail.com>
2024-05-14 13:26:13 +0300

    feat: functioning test_runner

--- test_runner.py
+++ test_runner.py
@@ -6,6 +6,8 @@ from colorama import Style
 import glob
 
 colorama_init(autoreset=True)
+LOGS_DIR = "logs"
+MVN_LOGFILE_PATH = os.path.join(LOGS_DIR, "mvn.logs")
 
 
 def log_info(msg):
@@ -18,6 +20,10 @@ def log_error(msg):
 
 def get_filename_without_extension(path):
     return os.path.splitext(os.path.basename(path))[0]
+
+
+def get_key_by_val(dictionary, val):
+    return list(dictionary.keys())[list(dictionary.values()).index(val)]
 
 
 def run_build(output_file=None):
@@ -40,19 +46,50 @@ def prerun_tests():
             subprocess.run(CMD.split(), stdin=stdin_handle, stdout=stdout_handle)
             log_info(f"Finished command: `{_cmd_str}`")
 
+    log_info(f"Finished pre_test")
+
 
 def run_tests():
+    log_info(f"Running tests")
+
+    successful_tests = 0
+    failing_tests = 0
     test_files = glob.glob("alan_tests/test_files/*")
     correct_out_files = glob.glob("alan_tests/correct_output/*")
+    test_files_map = {get_filename_without_extension(test): test for test in test_files}
+    correct_out_files_map = {get_filename_without_extension(out_file): out_file for out_file in correct_out_files}
     for test in test_files:
-        log_info(f"Found test file: `{get_filename_without_extension(test)}`")
+        test_title = get_key_by_val(test_files_map, test)
+        corresponding_out_file_path = correct_out_files_map.get(test_title)
+        log_info(f"{Fore.GREEN}{Style.BRIGHT}Checking test: `{test_title}`")
 
-    for correct_out in correct_out_files:
-        log_info(f"Found correct output file: `{get_filename_without_extension(correct_out)}`")
+        CMD: str = f"java -jar target/compiler-0.0.2.jar"
+        stdin = test
+        stdout = os.path.join(LOGS_DIR, f"_temp_{test_title}.out")
+        _cmd_str = f"{CMD} < {test} > {stdout}"
+        log_info(f"Running command: `{_cmd_str}`")
+        with open(stdin, "r") as stdin_handle, open(stdout, "w") as stdout_handle:
+            subprocess.run(CMD.split(), stdin=stdin_handle, stdout=stdout_handle)
+            log_info(f"Finished command: `{_cmd_str}`")
 
+        tmp_out_path = stdout
+        with open(tmp_out_path, "r") as test_handle, open(corresponding_out_file_path, "r") as correct_handle:
+            test_output_content = test_handle.read().rstrip()
+            correct_output_content = correct_handle.read().rstrip()
 
-LOGS_DIR = "logs"
-MVN_LOGFILE_PATH = os.path.join(LOGS_DIR, "mvn.logs")
+            if test_output_content == correct_output_content:
+                log_info(f"{Fore.GREEN}{Style.BRIGHT}Files match")
+                successful_tests += 1
+                os.remove(tmp_out_path)
+            else:
+                failing_tests += 1
+                log_error(f"Files don't match")
+                log_error(f"Correct output content: `{correct_output_content}`")
+                log_error(f"Test output content: `{test_output_content}`")
+
+    log_info(f"Finished tests")
+    return successful_tests, failing_tests
+
 
 if __name__ == "__main__":
     if not os.path.exists(LOGS_DIR):
@@ -69,5 +106,7 @@ if __name__ == "__main__":
             log_error(f"There was a problem with the Maven Build.")
             log_info(f"Exiting!")
             exit(1)
-
-    prerun_tests()
+    tests_passed, tests_failed = run_tests()
+    log_info(f"Test Passed: {Fore.GREEN}{Style.BRIGHT}{tests_passed}")
+    log_info(f"Test Failed: {Fore.RED}{Style.BRIGHT}{tests_failed}")
+    # prerun_tests()
